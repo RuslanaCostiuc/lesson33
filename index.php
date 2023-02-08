e6b7c0a41fa5ea2c5364a04577abc53be9f7b673
RuslanaCostiuc <114097325+RuslanaCostiuc@users.noreply.github.com>
2023-02-08 20:41:11 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html >
+<head>
+   
+    <title>Imagine Upload Using PHP</title>
+    <style>
+      body {
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction:column;
+        min-height:100vh;
+
+      }
+    
+ 
+    </style>
+</head>
+
+</html>
